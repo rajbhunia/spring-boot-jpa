d733c60f045cbffc4d3cb597df2207480bd3bdf8
Raj Bhunia <raj.curam@gmail.com>
2019-05-18 19:07:40 +0530

    Repository class for the Course entity is added.

---
+++ spring-jpa/src/main/java/com/bhuniar/jpa/repository/CourseRepository.java
@@ -0,0 +1,18 @@
+package com.bhuniar.jpa.repository;
+
+import com.bhuniar.jpa.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class CourseRepository {
+
+    @Autowired
+    EntityManager em;
+
+    public Course findById(Long id) {
+        return em.find(Course.class, id);
+    }
+}
