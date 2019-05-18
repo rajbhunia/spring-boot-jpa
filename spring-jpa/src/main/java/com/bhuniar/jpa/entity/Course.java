287850a111e3a84290aec0ba2084beb69281c759
Raj Bhunia <raj.curam@gmail.com>
2019-05-18 18:09:09 +0530

    Java 11 added

---
+++ spring-jpa/src/main/java/com/bhuniar/jpa/entity/Course.java
@@ -0,0 +1,34 @@
+package com.bhuniar.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    public Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
